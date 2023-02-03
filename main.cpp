9e31f9017fd0af8390a20a1cc7bd47ce1a60f0be
Farjahan Akter Boby <93110981+farjahanboby@users.noreply.github.com>
2023-02-03 23:04:05 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int i,n,cou=0;
+    string m,p;
+    cin>>n;
+    p='codeforces';
+    for(i=0;i<n;i++){
+            cin>>m;
+    if(m=="c"||m=="o"||m=="d"||m=="e"||m=="f"||m=="o"||m=="r"||m=="c"||m=="e"||m=="s")
+    cout <<"YES" << endl;
+    else
+        cout <<"NO" << endl;
+    }
+
+
+}
